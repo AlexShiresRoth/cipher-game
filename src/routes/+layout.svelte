a15e37d8da5cca4f536f136f6e34361b414e4bbb
Alex Roth <alexroth96@gmail.com>
2025-11-10 18:18:50 -0500

    Fixed bug when completing game on max guesses. Added how to play

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,35 @@
 	import '../app.css';
 
 	let { children } = $props();
+	const version = `0.3.1`;
 </script>
 
 {@render children()}
+
+<footer class="flex w-full flex-col items-center bg-gray-100">
+	<div class="flex w-full items-center border-b border-b-gray-200 px-4 py-2">
+		<p class="text-xs text-gray-400">
+			&copy; {new Date().getFullYear()} Alex Rothenberg. All rights reserved.
+		</p>
+	</div>
+	<div class="flex w-full items-center justify-between border-b border-b-gray-200 px-4 py-2">
+		<div>
+			<p class="text-xs text-gray-400">
+				created by <a
+					href="https://alexshiresroth.com/"
+					class="underline"
+					target="_blank"
+					rel="noopener noreferrer">Alex Rothenberg</a
+				>
+			</p>
+		</div>
+		<div>
+			<p class="text-xs text-gray-400">v{version}@alpha</p>
+		</div>
+	</div>
+	<div class="flex w-full items-center justify-center px-4 py-2">
+		<p class="text-xs text-gray-400">
+			Unauthorized copying, reproduction, or distribution of this game is prohibited.
+		</p>
+	</div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,8 +15,9 @@
 	export let moveLimit = data.maxAttempts;
 	export let moveAmount = 0;
 	export let modalOpen = false;
+	export let showNavModal = false;
 
-	const alpha = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	const alpha = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 
 	const today = format(parseISO(data.date), 'EEE d, yyyy');
 	const date = new Date();
@@ -24,6 +25,8 @@
 
 	let selected: string[] = [];
 	let guesses: string[] = [];
+
+	let indexToSwap: number;
 
 	function onSelect(letter: string) {
 		if (gameOver) return;
@@ -35,41 +38,9 @@
 	}
 	function clearSelection() {
 		selected = [];
+		indexToSwap = -1;
 	}
-	async function isValidWord(word: string) {
-		const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-
-		return res.ok;
-	}
-	async function guess() {
-		if (guesses.filter((guess) => guess === selected.join('')).length > 0) {
-			errors = [...errors, 'Already guessed'];
-			console.error('Already guessed!');
-			selected = [];
-			return;
-		}
-		if (selected.length === 1) {
-			errors = [...errors, 'Too short'];
-			console.error('Too short!');
-			selected = [];
-			return;
-		}
-		if (!cipherState.includes(selected[0])) {
-			errors = [...errors, 'Not in cipher'];
-			console.error('Not in cipher!');
-			selected = [];
-			return;
-		}
-
-		const isValidGuess = await isValidWord(selected.join(''));
-
-		if (!isValidGuess) {
-			errors = [...errors, 'Not a valid guess'];
-			console.error('Not a valid word in list');
-			selected = [];
-			return;
-		}
-
+	function getMoveToIndex() {
 		let selectionLength = selected.length;
 
 		const startIndex = cipherState.indexOf(selected[0]);
@@ -83,10 +54,53 @@
 			}
 		})();
 
+		return { moveIndex, startIndex };
+	}
+	async function isValidWord(word: string) {
+		const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+
+		return res.ok;
+	}
+	async function guess() {
+		if (guesses.filter((guess) => guess === selected.join('')).length > 0) {
+			errors = [...errors, 'Already guessed'];
+			console.error('Already guessed!');
+			selected = [];
+			indexToSwap = -1;
+			return;
+		}
+		if (selected.length === 1) {
+			errors = [...errors, 'Too short'];
+			console.error('Too short!');
+			selected = [];
+			indexToSwap = -1;
+			return;
+		}
+		if (!cipherState.includes(selected[0])) {
+			errors = [...errors, 'Not in cipher'];
+			console.error('Not in cipher!');
+			selected = [];
+			indexToSwap = -1;
+			return;
+		}
+
+		const isValidGuess = await isValidWord(selected.join(''));
+
+		if (!isValidGuess) {
+			errors = [...errors, 'Not a valid guess'];
+			console.error('Not a valid word in list');
+			selected = [];
+			indexToSwap = -1;
+			return;
+		}
+
+		const { moveIndex, startIndex } = getMoveToIndex();
+
 		if (moveIndex === startIndex) {
 			errors = [...errors, 'Same position'];
 			console.error('Move equates to same position');
 			selected = [];
+			indexToSwap = -1;
 			return;
 		}
 
@@ -98,12 +112,13 @@
 
 		guesses = [...guesses, selected.join('')];
 		selected = [];
+		indexToSwap = -1;
 		moveAmount++;
 	}
 
 	async function shareResults() {
 		const shareText = win
-			? `I cracked the Cipher in ${moveAmount}/${moveLimit}`
+			? `I cracked the Cipher in ${moveAmount}/${moveLimit} moves 😉`
 			: `I couldn't figure out the cipher today 😩`;
 
 		navigator.share({
@@ -112,7 +127,6 @@
 			url: 'https://cipher-game-alpha.vercel.app/'
 		});
 	}
-
 	$: (() => {
 		if (typeof window === 'undefined') return;
 
@@ -129,7 +143,7 @@
 			localStorage.setItem('moves', String(moveAmount));
 			localStorage.setItem('cipher', word);
 		}
-		if (moveAmount >= moveLimit) {
+		if (moveAmount > moveLimit) {
 			lose = true;
 			gameOver = true;
 			modalOpen = true;
@@ -161,10 +175,14 @@
 		if (storedCipher) {
 			cipherState = storedCipher.split('');
 		}
+		if (selected.length > 1) {
+			const { moveIndex } = getMoveToIndex();
+			indexToSwap = moveIndex;
+		}
 	})();
 </script>
 
-<main class="flex w-screen flex-col items-center">
+<main class="mb-28 flex w-screen flex-col items-center">
 	<nav class="grid w-full grid-cols-3 justify-between border-b border-b-black">
 		<div class="flex items-center justify-start px-4 text-sm text-black/50">
 			<p>{today}</p>
@@ -172,14 +190,35 @@
 		<div class="flex items-center justify-center">
 			<h2 class="text-3xl font-bold uppercase">CIPHER</h2>
 		</div>
-		<div class="flex items-center justify-end">
+		<div
+			class="relative flex items-center justify-end"
+			on:mouseleave={() => {
+				showNavModal = false;
+			}}
+			role="button"
+			tabindex="0"
+		>
 			<button
 				on:click={() => {
-					modalOpen = true;
+					showNavModal = true;
 				}}
 				class="bg-black px-4 py-2 text-white transition-colors hover:cursor-pointer hover:bg-black/80"
-				>{gameOver ? 'share' : 'menu'}</button
+				>{'menu'}</button
 			>
+			{#if showNavModal}
+				<div
+					class="absolute top-full left-0 z-10 flex w-full flex-col items-start bg-black p-2 text-white"
+				>
+					<a href="/how-to">how to play</a>
+					{#if gameOver}
+						<button
+							on:click={() => {
+								modalOpen = true;
+							}}>share</button
+						>
+					{/if}
+				</div>
+			{/if}
 		</div>
 	</nav>
 
@@ -249,13 +288,29 @@
 		<div class="mb-8 flex w-full justify-center gap-2">
 			{#each cipherState as key, i (`${key}-${i}`)}
 				<div
-					transition:fly={{ y: 100 }}
-					class={clsx('flex w-12 items-center justify-center border-2 p-2 md:w-20', {
-						'border-emerald-500 text-emerald-500': word.split('').indexOf(key) === i,
-						'border-black': word.split('').indexOf(key) !== i
-					})}
+					transition:fly={{ y: 100, duration: 700, delay: 100 * (i + 1) }}
+					class={clsx(
+						'relative flex w-12 items-center justify-center border-2 p-2 transition-all md:w-20',
+						{
+							'border-amber-500 text-amber-500': cipherState[i] === selected[0],
+							'border-indigo-500 text-indigo-500 shadow-[3px_3px_0px_1px_rgba(0,0,0,.2)]':
+								indexToSwap === i,
+							'border-emerald-500 text-emerald-500': word.split('').indexOf(key) === i,
+							'border-black': word.split('').indexOf(key) !== i && cipherState[i] !== selected[0]
+						}
+					)}
 				>
 					<p class="text-2xl font-bold uppercase md:text-5xl">{key}</p>
+					{#if selected.length > 0 && cipherState[i] === selected[0]}
+						<div class="absolute -bottom-6 z-10" transition:fly={{ y: 20 }}>
+							<span class="text-xs text-amber-500 uppercase">{cipherState[indexToSwap]}</span>
+						</div>
+					{/if}
+					{#if indexToSwap === i}
+						<div class="absolute -bottom-6 z-10" transition:fly={{ y: 20 }}>
+							<span class="text-xs text-indigo-500 uppercase">{selected[0]}</span>
+						</div>
+					{/if}
 				</div>
 			{/each}
 		</div>
@@ -316,31 +371,4 @@
 			</div>
 		{/if}
 	</div>
-	<footer class="mt-8 flex w-full flex-col items-center bg-gray-100">
-		<div class="flex w-full items-center border-b border-b-gray-200 px-4 py-2">
-			<p class="text-xs text-gray-400">
-				&copy; {new Date().getFullYear()} Alex Rothenberg. All rights reserved.
-			</p>
-		</div>
-		<div class="flex w-full items-center justify-between border-b border-b-gray-200 px-4 py-2">
-			<div>
-				<p class="text-xs text-gray-400">
-					created by <a
-						href="https://alexshiresroth.com/"
-						class="underline"
-						target="_blank"
-						rel="noopener noreferrer">Alex Rothenberg</a
-					>
-				</p>
-			</div>
-			<div>
-				<p class="text-xs text-gray-400">v0.2@alpha</p>
-			</div>
-		</div>
-		<div class="flex w-full items-center justify-center px-4 py-2">
-			<p class="text-xs text-gray-400">
-				Unauthorized copying, reproduction, or distribution of this game is prohibited.
-			</p>
-		</div>
-	</footer>
 </main>
