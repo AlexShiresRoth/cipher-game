7125684aeadea03c9930739bfb0347111983ad63
Alex Roth <alexroth96@gmail.com>
2025-11-16 11:05:29 -0500

    why is the app broken

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,9 +5,6 @@ import * as schema from './schema';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = postgres(env.DATABASE_URL, {
-	ssl: 'require',
-	prepare: false
-});
+const client = postgres(env.DATABASE_URL);
 
 export const db = drizzle(client, { schema });
