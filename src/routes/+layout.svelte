e63f8c2ab8657bd7820512288850bd20819cdc06
Alex Roth <alexroth96@gmail.com>
2025-11-21 20:58:39 -0500

    add new game mechanice for making letters unavailable and replenishable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 
 	let { children } = $props();
-	const version = `0.5.2`;
+	const version = `0.6.0`;
 </script>
 
 <main class="mb-28 flex min-h-[90vh] w-screen flex-col items-center overflow-x-hidden">
