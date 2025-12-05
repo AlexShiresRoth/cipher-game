bf139c7cdba7e99671e03940f776278fbeb68a4e
Alex Roth <alexroth96@gmail.com>
2025-12-04 19:00:00 -0500

    Add a todo

--- src/routes/api/cron/+server.ts
+++ src/routes/api/cron/+server.ts
@@ -22,6 +22,7 @@ const DAYS_OF_THE_WEEK = [
 	'Saturday'
 ];
 
+// TODO — I want this to run based on the last puzzles date, not the day the cron runs
 export const GET = async ({ request, url }) => {
 	const secret = url.searchParams.get('token')?.toString() || request.headers.get('Authorization');
 
