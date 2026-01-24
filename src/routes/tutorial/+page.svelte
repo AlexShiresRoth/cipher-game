137fcc5b695f5c65f447ae7d6a50bf3e1bf92489
Alex Roth <alexroth96@gmail.com>
2026-01-23 21:28:36 -0500

    Add better tutorial text

--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -96,8 +96,7 @@
 <div class="flex min-h-screen w-full flex-col justify-center gap-4 px-5 py-16 md:w-2/3 lg:w-1/2">
 	{#if !started}
 		<h1 class="text-2xl md:text-4xl">
-			Welcome to the Cipher tutorial, we'll be going step by step, going over how to decipher the
-			puzzle.
+			Welcome to the Cipher tutorial. Let’s go step by step and learn how to decipher the puzzle.
 		</h1>
 		<div>
 			<button
