a37f7dfb269b3f2cb9f08b11a07af7b3f3b318ae
Alex Roth <alexroth96@gmail.com>
2026-06-20 18:41:00 -0400

    schema update

--- drizzle/0001_amusing_slyde.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "cipherPuzzleV3" RENAME TO "cipherPuzzleV2";--> statement-breakpoint
-ALTER TABLE "cipherPuzzleV2" DROP CONSTRAINT "cipherPuzzleV3_word_unique";--> statement-breakpoint
-ALTER TABLE "cipherPuzzleV2" DROP CONSTRAINT "cipherPuzzleV3_date_unique";--> statement-breakpoint
-ALTER TABLE "cipherPuzzleV2" ADD CONSTRAINT "cipherPuzzleV2_word_unique" UNIQUE("word");--> statement-breakpoint
-ALTER TABLE "cipherPuzzleV2" ADD CONSTRAINT "cipherPuzzleV2_date_unique" UNIQUE("date");
